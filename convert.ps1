66a58c345a1a31ccbd9a76e23a3b3e07fa68e57e
Akmal <akhdarulakmal@students.undip.ac.id>
2026-05-18 18:53:35 +0700

    cleaning file

--- convert.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$bmp = [System.Drawing.Image]::FromFile("$PSScriptRoot\assets\icons\partners\wecon.bmp")
-$bmp.Save("$PSScriptRoot\assets\icons\partners\wecon.png", [System.Drawing.Imaging.ImageFormat]::Png)
-$bmp.Dispose()
-Write-Host "Converted wecon.bmp to wecon.png"
